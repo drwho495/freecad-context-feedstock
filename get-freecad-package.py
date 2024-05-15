6c0aca95fe49eaaadccadb18600f7c175fbd9d29
drwho495 <70452450+drwho495@users.noreply.github.com>
2024-05-14 20:15:40 -0500

    Update get-freecad-package.py

--- get-freecad-package.py
+++ get-freecad-package.py
@@ -3,6 +3,7 @@ import os
 def find_file(directory, prefix, suffix):
     for root, dirs, files in os.walk(directory):
         for file in files:
+            print(file)
             if file.startswith(prefix) and file.endswith(suffix):
                 return os.path.join(root, file)
     return None
