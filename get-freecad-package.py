2b56b47a5fb0a7b101bbc7b80b3d81c8629bcfd5
drwho495 <70452450+drwho495@users.noreply.github.com>
2024-05-14 20:08:13 -0500

    Create get-freecad-package.py

---
+++ get-freecad-package.py
@@ -0,0 +1,14 @@
+import os
+
+def find_file(directory, prefix, suffix):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.startswith(prefix) and file.endswith(suffix):
+                return os.path.join(root, file)
+    return None
+
+directory = "./build_artifacts/linux-64"
+prefix = "freecad"
+suffix = ".conda"
+
+print(find_file(directory, prefix, suffix))
